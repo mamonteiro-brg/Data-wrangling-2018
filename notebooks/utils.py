a70cfa2b6c55cb183e8ec788c8b2877be41fe69a
ruimgf <ruimgf95@gmail.com>
2018-06-24 12:26:19 +0100

    first submission

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,6 +1,24 @@
+import pandas as pd
+
+
 def clean_api_df(df,drop_columns=None):
 	df = df.copy()
 	if drop_columns is None:
 		return df
 	else:
 		return df.drop(columns=drop_columns)
+
+
+def join_target_api(target_df,api_df):
+	return target_df.join(api_df)
+
+def join_to_sql(target_df,api_df):
+	return target_df.join(api_df)		
+
+def prepare_dataset(df):
+	df = df.copy()
+	drop_columns = ['DEF_60_CNT_SOCIAL_CIRCLE','BASEMENTAREA_AVG','ENTRANCES_AVG','FONDKAPREMONT_MODE','OWN_CAR_AGE','name_education_type','name_income_type']
+	df = df.drop(columns=drop_columns)
+	df['flag_document_9'] = df.flag_document_9.astype('int')
+	df['flag_document_13'] = df.flag_document_13.astype('int')
+	return df
